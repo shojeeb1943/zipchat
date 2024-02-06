c74ed86d54a77620a73b9428e37d50e988a52d8a
Md Reyajul Islam <shojeeb1943@gmail.com>
2024-02-06 10:50:27 +0600

    edit from mac

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-//*************   © Copyrighted by Thinkcreative_Technologies. An Exclusive item of Envato market. Make sure you have purchased a Regular License OR Extended license for the Source Code from Envato to use this product. See the License Defination attached with source code. *********************
+//*****  © Copyrighted by Thinkcreative_Technologies. An Exclusive item of Envato market. Make sure you have purchased a Regular License OR Extended license for the Source Code from Envato to use this product. See the License Defination attached with source code. *********************
 
 import 'dart:core';
 import 'dart:io';
